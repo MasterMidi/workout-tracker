673ede22a25c981348db23cd57e891355226208f
Michael Andreas Graversen <home@michael-graversen.dk>
2025-07-29 02:32:14 +0200

    #25 add code styling config (#34)
    * init editorconfig
    
    * remove reset script
    
    * add schema for app.json file with expo
    
    * correct version and specify jsEngine
    
    * Ignore shellcheck on envrc files
    
    * add prettier and eslint styling configs
    
    * fix expo build properties

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,26 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import expoConfig from 'eslint-config-expo/flat';
+import jsdoc from 'eslint-plugin-jsdoc';
+import { defineConfig } from 'eslint/config';
 
-module.exports = defineConfig([
+export default defineConfig([
+  jsdoc.configs['flat/recommended-typescript'],
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      'jsdoc/require-jsdoc': [
+        'warn', // Show a warning for missing documentation
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: true,
+            FunctionExpression: true,
+          },
+        },
+      ],
+    },
   },
 ]);
